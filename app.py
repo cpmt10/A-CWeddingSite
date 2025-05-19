046cc2a654c07e56e3f2da4dceaef185c1bb1e91
GabrielR1121 <gr.garcia1121@gmail.com>
2025-05-18 22:12:42 -0400

    Moved files to website folder. Created gateway files for flask to start. add support files

--- app.py
+++ app.py
@@ -1,33 +1,12 @@
+from website import create_app
+"""
+Entrance point of application. When running this Web App run it thorugh here
+"""
 
-##
-# Flask script to connect to the DB and get guest data.
-##
-import os
-from flask import Flask, render_template, request, Response, redirect, url_for, session
+app = create_app()
 
-# Create flask object
-app = Flask(__name__)
 
-@app.route('/landing_validation', methods=['GET', 'POST'])
-def landing_validation():
-    if request.method == 'POST':
-        password = request.form.get('password')
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=False)
 
-        ## Placeholder: we need to check if the password is a valid id in the database (exists)
-        ## if it's not -> error, stay in landing.html
-        ## if it is -> redirect and gather the guest data.
-        headGuestId= "placeholderId"
-        app.secret_key = headGuestId
-        ## Right now just checking if true to keep progam running -> to be fixed.
-        if (True):
-            return redirect('get_party')
-
-        ## else send error message on landing screen
-
-@app.route('/get_party')
-def get_party():
-    session.get('headGuestId')
-    
-    ## Placeholder as well
-    partyList = []
-    return render_template('index.html', partyList=partyList)
+   
