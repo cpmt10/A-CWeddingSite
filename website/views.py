5514b5a2b2b184951bb5b91e8c0d1ba1c1453ba1
GabrielR1121 <gr.garcia1121@gmail.com>
2025-05-18 22:14:49 -0400

    created view page to handle all routing needs for the website

---
+++ website/views.py
@@ -0,0 +1,45 @@
+##
+# Flask script to connect to the DB and get guest data.
+##
+import os
+from flask import (
+    Flask,
+    render_template,
+    request,
+    Response,
+    redirect,
+    url_for,
+    session,
+    Blueprint,
+)
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/", methods=["GET", "POST"])
+def landing_validation():
+    if request.method == "POST":
+        password = request.form.get("password")
+        print(password)
+
+        ## Placeholder: we need to check if the password is a valid id in the database (exists)
+        ## if it's not -> error, stay in landing.html
+        ## if it is -> redirect and gather the guest data.
+        headGuestId = "placeholderId"
+        views.secret_key = headGuestId
+        ## Right now just checking if true to keep progam running -> to be fixed.
+        if True:
+            return redirect("get_party")
+    else:
+        return render_template("landing.html")
+
+    ## else send error message on landing screen
+
+
+@views.route("/get_party")
+def get_party():
+    session.get("headGuestId")
+
+    ## Placeholder as well
+    partyList = []
+    return render_template("index.html", partyList=partyList)
