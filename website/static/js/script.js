c82fad8181a6f021ee2f4b29ef99bcd87f9dc505
claudiapao10 <claudiapaolamonge10@gmail.com>
2025-05-23 16:21:05 -0400

    Updated landing logo - centered, navbar css

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -1,15 +1,26 @@
-const toggle = document.getElementsByClassName('toggle-button')[0]
-const navLinks = document.getElementsByClassName('nav-links')[0]
+/** Active and inactive responsive nav bar menu */
 
-toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active')
-})
+const toggle = document.getElementsByClassName("toggle-button")[0];
+const navLinks = document.getElementsByClassName("nav-links")[0];
+const contentArea = document.getElementById("contentArea")[0];
 
+toggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  navLinks.classList.toggle("active");
+  contentArea.classList.toggle("shifted");
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    if (navLinks.classList.contains("active")) {
+      navLinks.classList.remove("active");
+    }
+  });
+});
 
 /** Countown */
 var countDownDate = new Date("Dec 27, 2025 15:37:25").getTime();
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
 
@@ -17,10 +28,10 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
+
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown ").innerHTML = "EXPIRED";
