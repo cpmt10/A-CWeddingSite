cab8ecc463042bbf6aa5b44f9881ecaf6b420573
GabrielR1121 <gr.garcia1121@gmail.com>
2025-06-08 23:22:36 -0400

    Manually Overrode Manually overrode the CSS to prevent the pop-up from shifting items on the page.

--- website/static/js/alerts.js
+++ website/static/js/alerts.js
@@ -4,6 +4,8 @@ function errorPopup(message) {
         title: 'Oops...',
         text: message,
         iconColor: '#6a6926',
-        confirmButtonColor: '#6a6926'
+        confirmButtonColor: '#6a6926',
+        scrollbarPadding: false,
+        heightAuto: false,
     });
 }
