3cf13cbc5623656cd9259b6a5a52fcf91c00ce8f
claudiapao10 <claudiapaolamonge10@gmail.com>
2025-09-19 23:39:01 -0400

    db config

--- website/config.py
+++ website/config.py
@@ -5,5 +5,5 @@ import os
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     DB_PATH = os.path.join(BASE_DIR, "data", "Wedding.db")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_PATH
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
